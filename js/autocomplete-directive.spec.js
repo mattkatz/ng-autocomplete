d48eb9c9ae96ba6d51572551534fbf63ee9bc4e5
Matt Katz <git@morelightmorelight.com>
2014-02-06 15:18:09 -0500

    tests aren't even working yet.

--- js/autocomplete-directive.spec.js
+++ js/autocomplete-directive.spec.js
@@ -1,16 +1,18 @@
 describe("Autocomplete-directive", function(){
   var $compile,$rootScope;
+  var $scope;
+  var html;
   beforeEach(function(){
     //create a new autcomplete directive
     angular.mock.module("autocomplete-directive");
   });
   beforeEach(function(){
-    /*inject(function(_$compile_,_$rootScope_){
+    inject(function(_$compile_,_$rootScope_){
       $compile=_$compile_;
-      $rootScope = _$rootScope_.$new();
-    });*/
+      $scope = _$rootScope_.$new();
+    });
    html = "<mk-autocomplete ></mk-autocomplete>";
-   inject(function($compile,$rootScope){
+   /*inject(function($compile,$rootScope){
      $scope=$rootScope.$new();
      elem = angular.element(html);
      var linkFunc = $compile(elem);
@@ -18,11 +20,17 @@ describe("Autocomplete-directive", function(){
      scope = elem.scope();
      scope.$apply();
 
-   });
+   });*/
   });
   it("should create a textbox", function(){
     //compile the directive, there should be a textbox
-    expect( false).toBeTruthy();
+    template = $compile(html)($scope);
+    $scope.digest();
+    //Render the template as a string
+    var templateAsHtml = template.html();
+    expect(templateAsHtml).toContain("input");
+
+    //expect( false).toBeTruthy();
   });
   describe("when given an array as the lookup source", function(){
     //add characters to the input
