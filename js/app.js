3c680e24761eb244ce29fe309a2cc838e978f77f
Matt Katz <git@morelightmorelight.com>
2014-02-18 17:16:41 -0500

    fixed disconnect between inputValue and ngModel

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 var demo = angular.module('autocompleteDemo',['autocomplete-directive'])
 .controller('tagsCtrl', function($scope){
   $scope.tagSource = ['art','apples','politics','police', 'lorem','ipsum'];
-  $scope.tags = 'loquitor';
+  $scope.tags = '';
 
 }) 
   
